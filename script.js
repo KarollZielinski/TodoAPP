8557d769f5324e005f19feadf1c8ecc8af8a5239
KarollZielinski <123296044+KarollZielinski@users.noreply.github.com>
2023-05-09 20:57:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-const form = document.getElementById('form');
+const form = document.getElementById("form");
 const input = document.getElementById('input');
+const todos = document.getElementById("todos");
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    const todoText = input.value;
+
+    if(todoText){
+        const todoEL = document.createElement
+        ('li');
+        todoEL.innerHTML = todoText;
+
+        todoEL.addEventListener('click', ()=> {
+            todoEL.classList.toggle('completed');
+        });
+
+        todoEL.addEventListener('contextmenu',
+        (e)=>{
+            e.preventDefault();
+
+            todoEL.remove();
+        })
+
+        todos.appendChild(todoEL);
+
+        input.value = "";
+    }
+});
 
